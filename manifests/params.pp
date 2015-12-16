11c43ce4a24c1eadb0c27855dd6d542448e0eaa6
Jordi Prats <jprats@systemadmin.es>
2015-12-16 14:27:28 +0100

    inicial

---
+++ manifests/params.pp
@@ -0,0 +1,41 @@
+class sysctl::params {
+  
+  case $::osfamily
+  {
+    'redhat':
+    {
+      case $::operatingsystemrelease
+      {
+        /^[5-6].*$/:
+        {
+          $sysctlreload='sysctl -p'
+        }
+        /^7.*$/:
+        {
+          $sysctlreload='sysctl --system'
+        }
+        default: { fail("Unsupported RHEL/CentOS version! - $::operatingsystemrelease")  }
+      }
+    }
+    'Debian':
+    {
+      case $::operatingsystem
+      {
+      'Ubuntu':
+      {
+        case $::operatingsystemrelease
+        {
+          /^14.*$/:
+          {
+            $sysctlreload='sysctl --system'
+          }
+          default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
+        }
+      }
+      'Debian': { fail("Unsupported")  }
+      default: { fail("Unsupported Debian flavour!")  }
+      }
+    }
+    default: { fail("Unsupported OS!")  }
+  }
+}
---
+++ spec/classes/init_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+describe 'sysctl' do
+
+  context 'with defaults for all parameters' do
+    it { should contain_class('sysctl') }
+  end
+end
