ac3e72c4d3543ebe4e639ea1e090178513bf9b95
s-ntt <94382622+s-ntt@users.noreply.github.com>
2021-11-15 16:38:08 +0100

    Create interface.pp

---
+++ manifests/interface.pp
@@ -0,0 +1,11 @@
+define sysctl::interface($rp_filter, $arp_ignore, $arp_announce, $ifname=$name) {
+
+  include ::sysctl
+
+  concat::fragment { "${rp_filter} ${arp_ignore} ${arp_announce}":
+    target  => '/etc/sysctl.conf',
+    order   => '60',
+    content => template("${module_name}/interface.erb")
+  }
+
+}
